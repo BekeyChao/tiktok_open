711d3b556d9f8554a03ecda95380395d67584d5f
bekey <zhaojia0805@163.com>
2020-11-23 20:02:31 +0800

    1.1.21 添加brand list接口 ErrNoHandleConfig移动到合适位置

--- src/main/java/xyz/bekey/tiktokOpen/response/shop/ShopBrandListResponse.java
+++ src/main/java/xyz/bekey/tiktokOpen/response/shop/ShopBrandListResponse.java
@@ -1,6 +1,9 @@
 package xyz.bekey.tiktokOpen.response.shop;
 
+import xyz.bekey.tiktokOpen.domain.Brand;
 import xyz.bekey.tiktokOpen.response.TiktokOpenResponse;
 
-public class ShopBrandListResponse extends TiktokOpenResponse<Object> {
+import java.util.List;
+
+public class ShopBrandListResponse extends TiktokOpenResponse<List<Brand>> {
 }
